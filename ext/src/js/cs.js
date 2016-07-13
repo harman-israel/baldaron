9e2982478cfa35e89dc31fdba62f9f811057c3db
Asaf Atzmon <asaf.atzmon@harman.com>
2016-07-14 02:19:06 +0300

    insert parcel to db

--- ext/src/js/cs.js
+++ ext/src/js/cs.js
@@ -3,6 +3,10 @@ var tokenKey = "akfh5nf698mm2b5qysay8vxx5bb88b";
 var userKey = "u9utyrddiwb6iyqebqp8bdv8aqd3cc";
 var postUrl = "https://api.pushover.net/1/messages.json";
 
+var parcelsRef = new Firebase('https://baldaron-4adb5.firebaseio.com/parcels');
+
+
+
 
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
@@ -18,9 +22,27 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	$("#shipping-stateField select").find('option[value="'+request.state+'"]').attr("selected",true);
 
 	$(".continue-button").click(function(evt) {
+		// fill db
+		var baseurl = "https://baldaron-4adb5.firebaseio.com/";
+		var parcelData = {};
+		parcelData["description"] = $(".product-name a").attr("title");
+		parcelData["leecher"] = "sagiben";
+		parcelData["mule"] = request.username;
+		parcelData["mule_usernam"] = request.username;
+		parcelData["purchase_date"] = new Date();
+		parcelData["status"] = "Pending";
+		parcelData["total_price"] = $(".regular-price-container span").text();
+		parcelData["url"] = $(".product-image img").attr("src");
+		parcelId = Math.floor(Math.random() * 99) + 1 ; 
+		parcelStr = String.valueOf(parcelId);
+		parcel = {};
+		parcel[parcelId] = parcelData;
+	    parcelsRef.update(parcel);
+
+		// send notification
 		message = request.firstname + ", someone requests a delivery";
 		dataToSend = {"token":tokenKey,"user":userKey,"message":message};
-		$.post(postUrl,dataToSend);
+		//$.post(postUrl,dataToSend);
 	});
 	
 });
